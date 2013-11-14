bb5c163a12d1fdccfa8181058b0cd46c9d503e7e
Yen Tran <yen.tran@beaconinteractive.com>
2013-11-14 11:17:48 -0500

    Make private methods private; pass JSLint (yay); use promise after animation

--- javascripts/darkbox.js
+++ javascripts/darkbox.js
@@ -1,4 +1,5 @@
-Darkbox = {
+/*global window */
+var Darkbox = {
   KEY_CODES: { esc: 27, left: 37, right: 39 },
   _figs: [],
   _current_index: null,
@@ -13,13 +14,13 @@ Darkbox = {
       .on('click', '.db-next', Darkbox.next);
     $('.darkbox figure').each(function (index) {
       var $fig = $(this),
-          $link = $fig.find('.zoom'),
-          $img = $fig.find('img'),
-          src = $img.prop('src'),
-          caption = $fig.find('figcaption').text();
+        $link = $fig.find('.zoom'),
+        $img = $fig.find('img'),
+        src = $img.prop('src'),
+        caption = $fig.find('figcaption').text();
 
       Darkbox._figs.push({ src: src, caption: caption });
-      Darkbox.getImageSize($img, index, Darkbox.imageLoaded);
+      Darkbox._getImageSize($img, index, Darkbox._imageLoaded);
       $link.data('index', index); // save array index in data attribute
     });
     $('.zoom').click(Darkbox.zoom);
@@ -45,21 +46,22 @@ Darkbox = {
   display: function () {
     // console.log(Darkbox._figs)
     var $win = $(window),
-        $overlay = $('#db-overlay'),
-        $zoom = $overlay.find('.db-zoom-img'),
-        $img = $overlay.find('img'),
-        $caption = $overlay.find('figcaption'),
-        win_size = { w: $win.width(), h: $win.height() },
-        scaled_size = { w: Math.round(win_size.w * 0.75), h: Math.round(win_size.h * 0.75) },
-        fig = Darkbox._figs[Darkbox._current_index],
-        size;
+      $overlay = $('#db-overlay'),
+      $zoom = $overlay.find('.db-zoom-img'),
+      $img = $overlay.find('img'),
+      $caption = $overlay.find('figcaption'),
+      win_size = { w: $win.width(), h: $win.height() },
+      scaled_size = { w: Math.round(win_size.w * 0.75), h: Math.round(win_size.h * 0.75) },
+      fig = Darkbox._figs[Darkbox._current_index],
+      size;
+
     if (fig.isLandscape) {
-      size = (fig.size.w < scaled_size.w) 
-                ? fig.size 
+      size = (fig.size.w < scaled_size.w)
+                ? fig.size
                 : { w: scaled_size.w, h: Math.round(fig.size.h * scaled_size.w / fig.size.w) };
     } else {
-      size = (fig.size.h < scaled_size.h) 
-              ? fig.size 
+      size = (fig.size.h < scaled_size.h)
+              ? fig.size
               : { w: Math.round(fig.size.w * scaled_size.h / fig.size.h), h: scaled_size.h };
     }
     // console.log(fig.size)
@@ -68,34 +70,37 @@ Darkbox = {
     $zoom.css({
       'width': Darkbox._initial_size.w + 'px',
       'height': Darkbox._initial_size.h + 'px',
-      'margin-top': -1 * Darkbox._initial_size.h / 2 + 'px',      
+      'margin-top': -1 * Darkbox._initial_size.h / 2 + 'px',
       'margin-left': -1 * Darkbox._initial_size.w / 2 + 'px'
     });
-    if ($overlay.not(':visible'))
+    if ($overlay.not(':visible')) {
       $overlay.show().css('opacity', 1);
-    $zoom.animate({
-      width: size.w + 'px',
-      marginLeft: -1 * size.w / 2 + 'px'
-    }, Darkbox._duration)
-    .animate({
-      height: size.h + 'px',
-      marginTop: -1 * size.h / 2 + 'px',      
-    }, Darkbox._duration, function () {
-      $img.prop('src', fig.src).show().animate({ opacity: 1 }, Darkbox._duration * 2);
-      if (fig.caption.length > 0) {
-        $caption.text(fig.caption).show().animate({ opacity: 1 }, Darkbox._duration * 2);
-      }
-    });
+    }
+    $zoom
+      .animate({
+        width: size.w + 'px',
+        marginLeft: -1 * size.w / 2 + 'px'
+      }, Darkbox._duration)
+      .animate({
+        height: size.h + 'px',
+        marginTop: -1 * size.h / 2 + 'px'
+      }, Darkbox._duration)
+      .promise().done(function () {
+        $img.prop('src', fig.src).show().animate({ opacity: 1 }, Darkbox._duration * 2);
+        if (fig.caption.length > 0) {
+          $caption.text(fig.caption).show().animate({ opacity: 1 }, Darkbox._duration * 2);
+        }
+      });
     Darkbox._initial_size = size;
   },
-  getImageSize: function ($img, index, imgLoadedCallback) {
+  _getImageSize: function ($img, index, imgLoadedCallback) {
     $('<img/>').prop('src', $img.prop('src'))
       .load(function () {
         // console.log('load ' + this.width + ' ' + this.height)
         imgLoadedCallback(index, { w: this.width, h: this.height });
       });
   },
-  imageLoaded: function (index, size) {
+  _imageLoaded: function (index, size) {
     var fig = Darkbox._figs[index];
     fig.size = size;
     fig.isLandscape = size.w > size.h;
@@ -105,7 +110,9 @@ Darkbox = {
     event.stopPropagation();
     if (Darkbox._current_index !== null) {
       Darkbox._current_index++;
-      if (Darkbox._current_index >= Darkbox._figs.length) Darkbox._current_index = 0;
+      if (Darkbox._current_index >= Darkbox._figs.length) {
+        Darkbox._current_index = 0;
+      }
     }
     Darkbox.display();
   },
@@ -114,14 +121,16 @@ Darkbox = {
     event.stopPropagation();
     if (Darkbox._current_index !== null) {
       Darkbox._current_index--;
-      if (Darkbox._current_index < 0) Darkbox._current_index = Darkbox._figs.length - 1;
+      if (Darkbox._current_index < 0) {
+        Darkbox._current_index = Darkbox._figs.length - 1;
+      }
     }
     Darkbox.display();
   },
   zoom: function (event) {
     event.preventDefault();
     var $link = $(this),
-        index = parseInt($link.data('index'), 10);
+      index = parseInt($link.data('index'), 10);
     Darkbox._current_index = index;
     Darkbox.display();
   }
